b44dfe2b2b6554165d02220059055d7a419fa680
rotoblur <git@roto.win>
2022-12-02 15:09:02 +0900

    format day1.js

--- day1.js
+++ day1.js
@@ -6,9 +6,7 @@ const sortByCalroies = raw
 	.split("\n\n")
 	.map((a) => a.split("\n"))
 	.map((a) => a.reduce((o, v) => o + parseInt(v), 0))
-	.sort((a, b) => a - b)
-	.reverse()
-	.filter((a) => !isNaN(a));
+	.sort((a, b) => b - a);
 
 console.log(sortByCalroies[0]); // part one
 console.log(sortByCalroies[0] + sortByCalroies[1] + sortByCalroies[2]); // part two
