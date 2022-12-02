0abd7953a51d17226572382058bcec0b8a8a0634
rotoblur <git@roto.win>
2022-12-02 15:05:05 +0900

    Create day1.js

---
+++ day1.js
@@ -0,0 +1,14 @@
+// https://adventofcode.com/2022/day/1/input
+
+const raw = document.querySelector("body > pre").innerHTML.trim();
+
+const sortByCalroies = raw
+	.split("\n\n")
+	.map((a) => a.split("\n"))
+	.map((a) => a.reduce((o, v) => o + parseInt(v), 0))
+	.sort((a, b) => a - b)
+	.reverse()
+	.filter((a) => !isNaN(a));
+
+console.log(sortByCalroies[0]); // part one
+console.log(sortByCalroies[0] + sortByCalroies[1] + sortByCalroies[2]); // part two
